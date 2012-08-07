99186e5acc70e3c00925e459c9a7d21f41d5f96e
Ben <benbenben001@gmail.com>
2012-08-07 19:48:57 +0100

    changes tabs to spaces

--- src/main/java/org/bukkit/book/BookBuilder.java
+++ src/main/java/org/bukkit/book/BookBuilder.java
@@ -5,19 +5,19 @@ import org.bukkit.inventory.ItemStack;
  * A simple class to get an instance of org.bukkit.book.Book
  */
 public class BookBuilder {
-	
-	protected static BookBuilder instance = null;
-	
-	/**
-	 * Gets the Book from an ItemStack (must be Material.WRITTEN_BOOK)
-	 * @param itemstack
-	 * @return Book
-	 */
-	public Book getBook(ItemStack itemstack) {
-		if(instance == null) {
-			return null;
-		}
-		return instance.getBook(itemstack);
-	}
-	
+    
+    protected static BookBuilder instance = null;
+    
+    /**
+     * Gets the Book from an ItemStack (must be Material.WRITTEN_BOOK)
+     * @param itemstack
+     * @return Book
+     */
+    public Book getBook(ItemStack itemstack) {
+        if(instance == null) {
+            return null;
+        }
+        return instance.getBook(itemstack);
+    }
+    
 }
